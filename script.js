428d8393a29a1d445d0b93dad92a09d9256ba641
brentn <nesbitt.brent@gmail.com>
2016-11-18 11:43:49 -0800

    Make some changes according to jsLint

--- script.js
+++ script.js
@@ -1,50 +1,10 @@
-(function(){
+(function() {
   "use strict";
 
   var fontSize;
   var nightMode;
   var songs;
 
-  function init() {
-    setBackgroundColour();
-    fontSize=load("fontSize", 1);
-    nightMode=load("nightMode", false);
-    setFontSize();
-    setNightMode();
-    fetchJSONFile("./songs.json", function(response) {
-      songs = response.songs.sort(function(a, b) {
-        var x=a.title.toLowerCase();
-        var y=b.title.toLowerCase();
-        return x<y?-1:x>y?1:0;
-      });
-      showSong(0);
-      populateMenu();
-      openMenu();
-    });
-    setupClickListeners();
-  }
-
-  function setBackgroundColour() {
-    if (Math.random() < 0.5) { document.body.className+="alt"; }
-  }
-  function populateMenu() {
-    var songlist = document.getElementById("songlist");
-    songlist.innerHTML="";
-    for (var i = 0; i < songs.length; i++) {
-      var song = songs[i];
-      var menuItem=document.createElement("li");
-      menuItem.appendChild(document.createTextNode(song.title.substr(0,25)));
-      menuItem.id=i;
-      menuItem.addEventListener("click", showSong);
-      songlist.appendChild(menuItem);
-    };
-  }
-  function setupClickListeners() {
-    document.getElementById("menu-button").addEventListener("click", toggleMenu);
-    document.getElementById("nightmode").addEventListener("click", toggleNight);
-    document.getElementById("larger").addEventListener("click", increaseFont);
-    document.getElementById("smaller").addEventListener("click", decreaseFont);
-  }
   function load(key, defaultValue) {
     if (localStorage) {
       if (localStorage[key]) {return localStorage[key];}
@@ -55,19 +15,6 @@
     if (localStorage) {
       localStorage[key] = value;
     }
-  }
-  function fetchJSONFile(path, callback) {
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = function() {
-        if (httpRequest.readyState === 4) {
-            if (httpRequest.status === 200) {
-                var data = JSON.parse(httpRequest.responseText);
-                if (callback) { callback(data); }
-            }
-        }
-    };
-    httpRequest.open('GET', path);
-    httpRequest.send();
   }
   function openMenu() {
     var menu = document.getElementById('menu');
@@ -86,16 +33,18 @@
     var el = document.getElementById('song');
     document.getElementById('nightmode').checked = nightMode;
     if (nightMode) {
-      el.className += ' night'
+      el.className += ' night';
     } else {
-      el.className = el.className.replace('night',''\);
+      el.className = el.className.replace('night','');
     }
   }
   function setFontSize() {
     var lyrics = document.getElementById('lyrics');
     lyrics.style.fontSize=fontSize+"em";
   }
-
+  function setBackgroundColour() {
+    if (Math.random() < 0.5) { document.body.className+="alt"; }
+  }
   var showSong = function(evt) {
     var id = (evt && evt.target)?evt.target.id:0;
     var song = songs[id];
@@ -108,7 +57,7 @@
     document.getElementById('lyrics').innerHTML = lyrics;
     document.getElementById('author').innerHTML = song.author;
     closeMenu();
-  }
+  };
   var toggleMenu = function() {
     var menu = document.getElementById('menu');
     if (menu.className.indexOf('open')>-1) {
@@ -116,30 +65,74 @@
     } else {
       openMenu();
     }
-  }
+  };
   var toggleNight = function() {
     nightMode = !nightMode;
-    if (localStorage) {
-      localStorage.setItem('nightMode', nightMode);
-    }
+    save('nightMode', nightMode);
     setNightMode();
     closeMenu();
-  }
+  };
   var increaseFont = function() {
-    fontSize += .2;
-    if (localStorage) {
-      localStorage.setItem('fontSize', fontSize);
-    }
+    fontSize += 0.2;
+    save('fontSize', fontSize);
     setFontSize();
     closeMenu();
-  }
+  };
   var decreaseFont = function() {
-    fontSize -= .2;
-    if (localStorage) {
-      localStorage.setItem('fontSize', fontSize);
-    }
+    fontSize -= 0.2;
+    save('fontSize', fontSize);
     setFontSize();
     closeMenu();
+  };
+  function populateMenu() {
+    var songlist = document.getElementById("songlist");
+    songlist.innerHTML="";
+    var menuItem;
+    songs.forEach(function(song, i) {
+      menuItem=document.createElement("li");
+      menuItem.appendChild(document.createTextNode(song.title.substr(0,25)));
+      menuItem.id=i;
+      menuItem.addEventListener("click", showSong);
+      songlist.appendChild(menuItem);
+    });
+  }
+  function setupClickListeners() {
+    document.getElementById("menu-button").addEventListener("click", toggleMenu);
+    document.getElementById("nightmode").addEventListener("click", toggleNight);
+    document.getElementById("larger").addEventListener("click", increaseFont);
+    document.getElementById("smaller").addEventListener("click", decreaseFont);
+  }
+  function fetchJSONFile(path, callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                var data = JSON.parse(httpRequest.responseText);
+                if (callback) { callback(data); }
+            }
+        }
+    };
+    httpRequest.open('GET', path);
+    httpRequest.send();
+  }
+
+  function init() {
+    setBackgroundColour();
+    fontSize=load("fontSize", 1);
+    nightMode=load("nightMode", false);
+    setFontSize();
+    setNightMode();
+    fetchJSONFile("./songs.json", function(response) {
+      songs = response.songs.sort(function(a, b) {
+        var x=a.title.toLowerCase();
+        var y=b.title.toLowerCase();
+        return x<y?-1:x>y?1:0;
+      });
+      showSong(0);
+      populateMenu();
+      openMenu();
+    });
+    setupClickListeners();
   }
 
   init();
